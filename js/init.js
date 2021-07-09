f015fb6a3b67d6401e891c1bfde2631c323c11e9
VeriitoGA <87164514+VeriitoGA@users.noreply.github.com>
2021-07-09 01:26:10 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyCy2TabkMu1bvd-TaO5QAtuk9tSlGO8ghQ",
+    authDomain: "autentificavga.firebaseapp.com",
+    projectId: "autentificavga",
+    storageBucket: "autentificavga.appspot.com",
+    messagingSenderId: "307703512039",
+    appId: "1:307703512039:web:e2be6162d96d24768f9b42",
+    measurementId: "G-3CY9PZS645"
+});
